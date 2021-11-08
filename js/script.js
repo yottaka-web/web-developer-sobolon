a1c00876cf5285757b91c722f275544be49803ba
よったか <67794392+yottaka-web@users.noreply.github.com>
2021-11-08 12:17:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,28 @@ jQuery('.drawer-icon').on('click', function($) {
 jQuery('.contact-form__select').on('click', function () {
   jQuery('span').toggleClass('is-active');
 });
+
+
+jQuery('a[href^="#"]').on('click', function () {
+
+  var header = jQuery('.header').innerHeight();
+  var id = jQuery(this).attr('href');
+  var position = 0;
+  if ( id != '#' ) {
+    var position = jQuery(id).offset().top - header;
+  };
+  console.log(id);
+  console.log(position);
+  jQuery('body,html').animate({
+    scrollTop: position
+  }, 500);
+  return false;
+});
+jQuery(window).on('scroll', function() {
+  console.log();
+  if ( 100 < jQuery(this).scrollTop() ) {
+    jQuery('.to-top').addClass('is-show')
+  } else {
+    jQuery('.to-top').removeClass('is-show')
+  }
+});
